e4e5e17de4849308e68b37c5f16acb168434c262
neoki <ot07neoki@gmail.com>
2024-02-02 12:56:54 +0900

    feat: add sign in component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import { SignIn } from "@clerk/nextjs";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p>Hello Money Flows</p>
+      <SignIn />
     </main>
   );
 }
